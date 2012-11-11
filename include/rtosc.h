430c4db03abcf4ea895446ea7fdf74875dbece2d
fundamental <mark.d.mccurry@gmail.com>
2012-11-11 11:35:46 -0500

    Doc: add more API documentation
    - Overview of more C functions added to guide
    - Most C functions now have doxygen documentation

--- include/rtosc.h
+++ include/rtosc.h
@@ -1,4 +1,4 @@
-/** @file */
+/** @file rtosc.h */
 #ifndef RTOSC_H
 #define RTOSC_H
 #include <stdarg.h>
@@ -29,6 +29,17 @@ typedef union {
  * When buffer is NULL, the function returns the size of the buffer required to
  * store the message
  *
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * //Example messages
+ * char buffer[128];
+ * rtosc_message(buffer,128,"/path","TFI");
+ * rtosc_message(buffer,128,"/path","s","foobar");
+ * rtosc_message(buffer,128,"/path","i",128);
+ * rtosc_message(buffer,128,"/path","f",128.0);
+ * const char blob[4] = {'a','b','c','d'};
+ * rtosc_message(buffer,128,"/path","b",4,blob);
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *
  * @param buffer    Memory to write to
  * @param len       Length of buffer
  * @param address   OSC pattern to send message to
@@ -43,7 +54,7 @@ size_t rtosc_message(char   *buffer,
         ...);
 
 /**
- * See rtosc_message()
+ * @see rtosc_message()
  */
 size_t rtosc_vmessage(char   *buffer,
         size_t      len,
@@ -53,22 +64,30 @@ size_t rtosc_vmessage(char   *buffer,
 
 /**
  * Returns the number of arguments found in a given message
+ *
+ * @param msg well formed OSC message
+ * @returns number of arguments in message
  */
 unsigned rtosc_narguments(const char *msg);
 
 /**
- * Returns the type of the ith argument in msg
+ * @param msg well formed OSC message
+ * @param i   index of argument
+ * @returns the type of the ith argument in msg
  */
 char rtosc_type(const char *msg, unsigned i);
 
 /**
- * Returns an argument by value via the arg_t union
  * Blob data may be safely written to
+ * @param msg OSC message
+ * @param i   index of argument
+ * @returns an argument by value via the arg_t union
  */
 arg_t rtosc_argument(const char *msg, unsigned i);
 
 /**
- * Returns the size of a message given a chunk of memory.
+ * @param msg OSC message
+ * @returns the size of a message given a chunk of memory.
  */
 size_t rtosc_message_length(const char *msg);
 
@@ -82,36 +101,56 @@ typedef struct {
  * 
  * @param ring The addresses and lengths of the split buffer, in a compatible
  *             format to jack's ringbuffer
+ * @returns size of message stored in ring datastructure
  */
 size_t rtosc_message_ring_length(ring_t *ring);
 
 /**
- * Returns the argument string of a given message
+ * @param OSC message
+ * @returns the argument string of a given message
  */
 const char *rtosc_argument_string(const char *msg);
 
 /**
  * Generate a bundle from sub-messages
+ *
+ * @param buffer Destination buffer
+ * @param len    Length of buffer
+ * @param tt     OSC time tag
+ * @param elms   Number of sub messages
+ * @param ...    Messages
+ * @returns legnth of generated bundle or zero on failure
  */
 size_t rtosc_bundle(char *buffer, size_t len, uint64_t tt, int elms, ...);
-//TODO
 
 /**
  * Find the elements in a bundle
+ *
+ * @param msg OSC bundle
+ * @returns The number of messages contained within the bundle
  */
-size_t rtosc_bundle_elements(const char *buffer);
+size_t rtosc_bundle_elements(const char *msg);
 
 /**
  * Fetch a message within the bundle
+ *
+ * @param buffer OSC bundle
+ * @param i      index of sub message
+ * @returns The ith message within the bundle
  */
-const char *rtosc_bundle_fetch(const char *buffer, unsigned elm);
+const char *rtosc_bundle_fetch(const char *msg, unsigned i);
 
 /**
- * True if message is a bundle
+ * Test if the buffer contains a bundle
+ *
+ * @param msg OSC message
+ * @returns true if message is a bundle
  */
 int rtosc_bundle_p(const char *msg);
 
-//TODO TODO
+/**
+ * @returns Time Tag for a bundle
+ */
 uint64_t rtosc_bundle_timetag(const char *msg);
 
 #ifdef __cplusplus
