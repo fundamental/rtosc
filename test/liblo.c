2ec14e5b14acca5aa5e30b188fb87676856cb4b8
fundamental <mark.d.mccurry@gmail.com>
2015-04-01 10:53:13 -0400

    Increase General Build/Test Debug Info

--- test/liblo.c
+++ test/liblo.c
@@ -15,7 +15,6 @@ int main()
     //clean buffer
     memset(buffer, 0, sizeof(buffer));
 
-
     //generate liblo message
     size_t len = 128;
     lo_message message = lo_message_new();
@@ -24,11 +23,15 @@ int main()
     lo_message_serialise(message, "/path", buffer, &len);
     lo_message_free(message);
 
+    printf("Generating a message from liblo '/path\\0\\0\\0,fi\\0iiiiffff'...\n");
+
     good &= !strcmp("/path", buffer);
     good &= 24.0 == rtosc_argument(buffer, 0).f;
     good &= 42   == rtosc_argument(buffer, 1).i;
     good &= len  == rtosc_message_length(buffer, 128);
+    printf("Message sanity is currently '%d'\n", good);
 
+    printf("Generating a message from rtosc '/li\\0\\0,bb\\0iiiibbbbiiiibbbb'...\n");
     size_t len2 = rtosc_message(buffer2, 1024, "/li", "bb", 4, buffer, 4, buffer);
     for(size_t i=0; i<len2; i+=4)  {
         for(size_t j=i; j<len2 && j<i+4; ++j) {
@@ -51,16 +54,19 @@ int main()
     lo_message msg2 = lo_message_deserialise((void*)buffer2, len2, &result);
     good &= msg2 != NULL;
     if(msg2 == NULL)
-        printf("Bad result from liblo '%d'\n", result);
+        printf("Liblo Did Not Accept the Rtosc Message [error '%d']\n", result);
+    else
+        printf("Liblo Accpted The Rtosc Message\n");
 
+    printf("Generating a message from rtosc '/close-ui\\0\\0\\0,\\0\\0\\0'...\n");
     int len3 = rtosc_message(buffer3+4, 2048, "/close-ui", "");
     lo_message msg3 = lo_message_deserialise((void*)buffer3+4, len3, &result);
     printf("len3 = %d\n", len3);
     good &= msg2 != NULL;
     if(msg3 == NULL)
-        printf("Bad result from liblo '%d'\n", result);
+        printf("Liblo Did Not Accept the Rtosc Message [error '%d']\n", result);
     else
-        printf("Good result from liblo\n");
+        printf("Liblo Accpted The Rtosc Message\n");
 
     return !good;
 }
