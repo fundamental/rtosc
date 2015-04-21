d11b97a032b821ce96959e881258e319ed205a0e
Tobias Doerffel <tobias.doerffel@gmail.com>
2015-04-21 22:46:31 +0200

    test/empty-strings: fixed compilation for non-POSIX systems
    Use C89 functions only.

--- test/empty-strings.c
+++ test/empty-strings.c
@@ -1,6 +1,6 @@
 #include <rtosc/rtosc.h>
 #include <stdio.h>
-#include <err.h>
+#include <stdlib.h>
 
 char buffer[1024];
 
@@ -10,11 +10,17 @@ int main()
     size_t length = rtosc_message(buffer, 1024, "/path", "sss", "", "", "");
     // /pat h000 ,sss 0000 0000 0000 0000
     if(length != 28)
-        errx(1, "Bad length for empty strings...");
+    {
+        fprintf(stderr, "Bad length for empty strings...");
+        exit(1);
+    }
     if(!rtosc_argument(buffer, 0).s ||
             !rtosc_argument(buffer, 1).s ||
             !rtosc_argument(buffer, 2).s)
-        errx(1, "Failure to retreive string pointer...");
+    {
+        fprintf(stderr, "Failure to retreive string pointer...");
+        exit(1);
+    }
     printf("%p %p %p\n",
             rtosc_argument(buffer, 0).s,
             rtosc_argument(buffer, 1).s,
