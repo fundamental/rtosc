7fc500c4888425fc28fc5abb75c139b2a27f1df7
Ricard Wanderlof <polluxsynth@butoba.net>
2022-04-10 08:33:54 +0200

    thread_link: Add read lookahead
    Add read lookhead feature, whereby read operations can be set to read from
    a lookahead queue, which is the same as the ordinary read queue,
    except the messages are not considered removed from the ring, but
    remain for a future non-lookahead read to actually consume.
    
    The lookahead queue is synchronized to the ordinary read queue
    whenever an ordinary read is performed.

--- src/cpp/thread-link.cpp
+++ src/cpp/thread-link.cpp
@@ -13,15 +13,20 @@ struct internal_ringbuffer_t {
     char *buffer;
     std::atomic<off_t> write;
     std::atomic<off_t> read;
+    /* read_lookahead strictly speaking does not need to be atomic as it is
+     * only accessed from the read side, but it makes things easier if it's
+     * the same type as read.
+     */
+    std::atomic<off_t> read_lookahead;
     size_t size;
 };
 
 typedef internal_ringbuffer_t ringbuffer_t;
 
-static size_t ring_read_size(ringbuffer_t *ring)
+static size_t ring_read_size(ringbuffer_t *ring, bool lookahead)
 {
     const size_t w = ring->write;
-    const size_t r = ring->read;
+    const size_t r = lookahead ? ring->read_lookahead : ring->read;
 
     return (w-r+ring->size) % ring->size;
 }
@@ -50,28 +55,37 @@ static void ring_write(ringbuffer_t *ring, const char *data, size_t len)
     }
     ring->write = next_write;
 }
-static void ring_read(ringbuffer_t *ring, char *data, size_t len)
+static void ring_read(ringbuffer_t *ring, char *data, size_t len, bool lookahead)
 {
-    assert(ring_read_size(ring) >= len);
-    const off_t  next_read = (ring->read + len)%ring->size;
+    assert(ring_read_size(ring, lookahead) >= len);
+    const off_t  read = lookahead ? ring->read_lookahead : ring->read;
+    const off_t  next_read = (read + len)%ring->size;
 
     //discontinuous read
-    if(next_read < ring->read) {
-        const size_t r1 = ring->size - ring->read;
+    if(next_read < read) {
+        const size_t r1 = ring->size - read;
         const size_t r2 = len - r1;
-        memcpy(data,    ring->buffer+ring->read, r1);
-        memcpy(data+r1, ring->buffer,            r2);
+        memcpy(data,    ring->buffer+read, r1);
+        memcpy(data+r1, ring->buffer,      r2);
     } else { //contiguous
-        memcpy(data, ring->buffer+ring->read, len);
+        memcpy(data, ring->buffer+read, len);
     }
-    ring->read = next_read;
+    if (lookahead)
+        ring->read_lookahead = next_read;
+    else
+        /* When doing an ordinary read, synchronize lookahead pointer with
+         * read pointer, so that subsequent lookahead reads will start from
+         * the read pointer. This way, we guarantee that the lookahead
+         * queue is always equal to or shorter than the read queue.
+         */
+        ring->read_lookahead = ring->read = next_read;
 }
-static void ring_read_vector(ringbuffer_t *ring, ring_t *r)
+static void ring_read_vector(ringbuffer_t *ring, ring_t *r, bool lookahead)
 {
     assert(r);
-    size_t read_size = ring_read_size(ring);
-    off_t  read      = ring->read;
-    r[0].data = ring->buffer+ring->read;
+    size_t read_size = ring_read_size(ring, lookahead);
+    off_t  read      = lookahead ? ring->read_lookahead : ring->read;
+    r[0].data = ring->buffer+read;
     if(read_size+read > ring->size) { //discontinuous
         size_t r2 = (read_size+read)%ring->size;
         size_t r1 = read_size - r2;
@@ -92,10 +106,11 @@ ThreadLink::ThreadLink(size_t max_message_length, size_t max_messages)
     read_buffer(new char[MaxMsg]),
     ring(new ringbuffer_t)
 {
-    ring->buffer = new char[BufferSize];
-    ring->size   = BufferSize;
-    ring->read   = 0;
-    ring->write  = 0;
+    ring->buffer         = new char[BufferSize];
+    ring->size           = BufferSize;
+    ring->read           = 0;
+    ring->read_lookahead = 0;
+    ring->write          = 0;
     memset(write_buffer, 0, MaxMsg);
     memset(read_buffer, 0, MaxMsg);
 }
@@ -140,23 +155,54 @@ void ThreadLink::raw_write(const char *msg)
 /**
  * @returns true iff there is another message to be read in the buffer
  */
+bool ThreadLink::hasNext(bool lookahead) const
+{
+    return ring_read_size(ring, lookahead);
+}
+
+/**
+ * @returns true iff there is another message to be read in the buffer
+ */
 bool ThreadLink::hasNext(void) const
 {
-    return ring_read_size(ring);
+    return ThreadLink::hasNext(false);
+}
+
+/**
+ * @returns true iff there is another message to be read in the lookahead queue
+ */
+bool ThreadLink::hasNextLookahead(void) const
+{
+    return ThreadLink::hasNext(true);
+}
+
+/**
+ * Read a new message from the ringbuffer
+ * @lookahead: normally False, set to True when reading from the lookahead queue
+ */
+msg_t ThreadLink::read(bool lookahead) {
+    ring_t r[2];
+    ring_read_vector(ring,r,lookahead);
+    const size_t len =
+        rtosc_message_ring_length(r);
+    assert(ring_read_size(ring, lookahead) >= len);
+    assert(len <= MaxMsg);
+    ring_read(ring, read_buffer, len, lookahead);
+    return read_buffer;
 }
 
 /**
  * Read a new message from the ringbuffer
  */
 msg_t ThreadLink::read(void) {
-    ring_t r[2];
-    ring_read_vector(ring,r);
-    const size_t len =
-        rtosc_message_ring_length(r);
-    assert(ring_read_size(ring) >= len);
-    assert(len <= MaxMsg);
-    ring_read(ring, read_buffer, len);
-    return read_buffer;
+    return ThreadLink::read(false);
+}
+
+/**
+ * Read a new message from the ringbuffer along the lookahead queue
+ */
+msg_t ThreadLink::read_lookahead(void) {
+    return ThreadLink::read(true);
 }
 
 /**
