8790a53732dbc89c9bdd527a0517c1292528d9a0
Dan Church <amphetamachine@gmail.com>
2022-09-18 14:40:55 -0500

    Fix invalid comparison operator in .pc files

--- librtosc-cpp.pc.cmake
+++ librtosc-cpp.pc.cmake
@@ -9,6 +9,6 @@ includedir=@CMAKE_INSTALL_FULL_INCLUDEDIR@
 Name: rtosc_cpp
 Description: rtosc_cpp - a realtime safe open sound control serialization and dispatch system for C++
 Version: @VERSION_MAJOR@.@VERSION_MINOR@.@VERSION_PATCH@
-Requires: librtosc == @VERSION_MAJOR@.@VERSION_MINOR@.@VERSION_PATCH@
+Requires: librtosc = @VERSION_MAJOR@.@VERSION_MINOR@.@VERSION_PATCH@
 Libs: -L${libdir} -lrtosc -lrtosc-cpp
 Cflags: -I${includedir}
