1a2101afc9c7b00104c19e4d0a040c40a0fd72b4
Johannes Lorenz <j.git@lorenz-ho.me>
2023-11-04 19:56:01 +0100

    Fix non-reachable code (NDEBUG) warnings

--- src/cpp/arg-val-cmp.c
+++ src/cpp/arg-val-cmp.c
@@ -1,3 +1,27 @@
+/*
+ * Copyright (c) 2022-2024 Johannes Lorenz
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT.  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
 #include <assert.h>
 #include <string.h>
 #include <stdlib.h>
@@ -263,9 +287,9 @@ int rtosc_arg_vals_cmp_single(const rtosc_arg_val_t* _lhs,
             // no recovery: the programmer did not pass the right args, and
             // we don't have a function to compare ranges here
             assert(false);
-            exit(1);
             break;
         default:
+            rval = -1;
             assert(false);
     }
     else
