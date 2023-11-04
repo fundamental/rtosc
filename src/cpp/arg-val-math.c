04ecd17b7cc0a34bc5be6e6e6d4035a4004ea3b8
Johannes Lorenz <j.git@lorenz-ho.me>
2023-11-04 20:21:39 +0100

    Fix compiler warning "maybe uninitialized"

--- src/cpp/arg-val-math.c
+++ src/cpp/arg-val-math.c
@@ -1,3 +1,27 @@
+/*
+ * Copyright (c) 2022-2024 Johannes Lorenz
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT.  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
 #include <assert.h>
 #include <rtosc/arg-val-math.h>
 #include <rtosc/rtosc.h>
@@ -225,8 +249,9 @@ rtosc_arg_val_t* rtosc_arg_val_range_arg(const rtosc_arg_val_t *range_arg,
                                          int ith, rtosc_arg_val_t* result)
 {
     rtosc_arg_val_t n_as_arg_val, mult;
-    rtosc_arg_val_from_int(&n_as_arg_val, range_arg[1].type, ith);
-    rtosc_arg_val_mult(&n_as_arg_val, range_arg+1, &mult);
-    rtosc_arg_val_add(range_arg+2, &mult, result);
-    return result;
+    int ok = 1;
+    if(ok) ok = rtosc_arg_val_from_int(&n_as_arg_val, range_arg[1].type, ith);
+    if(ok) ok = rtosc_arg_val_mult(&n_as_arg_val, range_arg+1, &mult);
+    if(ok) ok = rtosc_arg_val_add(range_arg+2, &mult, result);
+    return ok ? result : NULL;
 }
