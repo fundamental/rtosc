3582784f9401fe2d7dd84a72067d1510106a6128
Ross maxx <mrroshan127@gmail.com>
2023-11-11 20:06:28 +0530

    revert the fix because pc file and not cmake

--- librtosc-cpp.pc.cmake
+++ librtosc-cpp.pc.cmake
@@ -10,9 +10,5 @@ Name: rtosc_cpp
 Description: rtosc_cpp - a realtime safe open sound control serialization and dispatch system for C++
 Version: @VERSION_MAJOR@.@VERSION_MINOR@.@VERSION_PATCH@
 Requires: librtosc = @VERSION_MAJOR@.@VERSION_MINOR@.@VERSION_PATCH@
-if(NOT MSVC)
-    Libs: -L${libdir} -lrtosc -lrtosc-cpp
-else
-    Libs: /LIBPATH:${libdir} -lrtosc -lrtosc-cpp
-endif()
+Libs: -L${libdir} -lrtosc -lrtosc-cpp
 Cflags: -I${includedir}
