2fdac895b02d0cd2c4f692983d727ade54101bde
Ross maxx <mrroshan127@gmail.com>
2023-11-11 20:17:35 +0530

    Revert "try at fixing linking"
    This reverts commit cd8f29bf3bbd7b0ba3c6f94ad1a4c601033d3154.

--- librtosc-cpp.pc.cmake
+++ librtosc-cpp.pc.cmake
@@ -5,12 +5,10 @@
 prefix=@CMAKE_INSTALL_PREFIX@
 libdir=@CMAKE_INSTALL_FULL_LIBDIR@
 includedir=@CMAKE_INSTALL_FULL_INCLUDEDIR@
-set(LIBS_FLAG "$<$<CXX_COMPILER_ID:MSVC>:/LIBPATH:${libdir}>$<$<NOT:$<CXX_COMPILER_ID:MSVC>>:-L${libdir}> -lrtosc -lrtosc-cpp")
-
 
 Name: rtosc_cpp
 Description: rtosc_cpp - a realtime safe open sound control serialization and dispatch system for C++
 Version: @VERSION_MAJOR@.@VERSION_MINOR@.@VERSION_PATCH@
 Requires: librtosc = @VERSION_MAJOR@.@VERSION_MINOR@.@VERSION_PATCH@
-set(Libs: ${LIBS_FLAG})
+Libs: -L${libdir} -lrtosc -lrtosc-cpp
 Cflags: -I${includedir}
