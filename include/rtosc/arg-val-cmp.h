670de9ae8da271b6934d00362b4927b8d9df814e
Johannes Lorenz <j.git@lorenz-ho.me>
2024-11-24 14:47:38 +0100

    Fixup Fix test file names

--- include/rtosc/arg-val-cmp.h
+++ include/rtosc/arg-val-cmp.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017-2023 Johannes Lorenz
+ * Copyright (c) 2017-2024 Johannes Lorenz
  *
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the "Software"),
